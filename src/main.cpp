2f6ec9997c356b709f12b22c0d58fedc934a4aae
KarenLSu <you@example.com>
2021-04-08 17:44:20 -0700

    display function test

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include <Arduino.h>
-#include <button.cpp>
-
-
+#include <red.cpp>
 
 void setup() {
   Serial.begin(9600);
   pinMode(displayButton, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(displayButton), toggleDisplay, FALLING);
+  pinMode(pot, INPUT);
+  lcd.begin(16, 2);
+  lcd.print("Cheryl, Emma, and");
+  lcd.setCursor(0,1);
+  lcd.print("Jake are cool");
 }
 
 void loop() {
-  //hey
+displayList();
 }
