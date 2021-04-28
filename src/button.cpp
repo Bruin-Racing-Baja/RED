75e174195d625feeb14bde252b29d5243b16483b
Gautier404 <and.jgautier@gmail.com>
2021-04-28 16:12:42 -0700

    Make things Pretty

--- src/button.cpp
+++ src/button.cpp
@@ -1,32 +1,42 @@
+//---------------------------------------------//
+// Button indexer set up and functions
+// Author: Karen Su & Drew Gautier
+// Date: 4/28/2021
+//---------------------------------------------//
+// This file contains the declarations for the Button
+// class constructor and it's member functions.
+
+
 #include <button.h>
+
 
 Button::Button (int PinsToRead, int dispButton) {
     m_displayButton = dispButton;
     m_timePressed = 0;
     m_pinBeingRead = 0;
     m_numberofPinstoRead = PinsToRead;
-    attachInterrupt(digitalPinToInterrupt(m_displayButton), this->toggleDisplay, FALLING);
+
+    //When button pin voltage falls the toggle display function is called
+    attachInterrupt(digitalPinToInterrupt(m_displayButton), this->toggle, FALLING);
 }
 //hi
 
+
+//Need to declare these variables here to make space in the memory for the interupt to work?
 unsigned long Button::m_timePressed;
 int Button::m_pinBeingRead;
 int Button::m_numberofPinstoRead;
 
-void Button::toggleDisplay() {
+
+//When called debounce button then increment pinBeingRead member variable if neccesary
+void Button::toggle() {
     if (millis() - m_timePressed > 200) {
         m_pinBeingRead++;
         
         if (m_pinBeingRead == m_numberofPinstoRead) {
           m_pinBeingRead = 0;
         }
-        Serial.print(m_pinBeingRead);
-        Serial.print(" ");
-        Serial.print(m_timePressed);
-        Serial.print(" ");
-        Serial.print(millis());
-        Serial.print(" ");
-        Serial.println(millis()-m_timePressed);
+
         m_timePressed = millis();
     }
 }
