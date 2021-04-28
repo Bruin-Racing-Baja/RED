8fd83517fd65b9ad9192cdb297e779d18e88db79
Gautier404 <and.jgautier@gmail.com>
2021-04-28 16:12:42 -0700

    Make things Pretty

--- include/button.h
+++ include/button.h
@@ -1,18 +1,29 @@
+//---------------------------------------------//
+// Button indexer set up and functions
+// Author: Karen Su & Drew Gautier
+// Date: 4/28/2021
+//---------------------------------------------//
+// This file contains the declarations for the Button
+// class constructor and it's member functions.
+
+
 #include <Arduino.h>
+
 
 #ifndef BUTTON_H
 #define BUTTON_H
 
+
 class Button{
     private:
-    int m_displayButton;
-    static unsigned long m_timePressed;
-    
-    static int m_numberofPinstoRead;
+    int m_displayButton;                    //The pin reading the button
+    static unsigned long m_timePressed;     //The last time the button was pressed in milliseconds, hense why it's a long
+    static int m_numberofPinstoRead;        //The number of indexes there will be
+
     public:
     Button(int pinsToRead, int dispButton);
-    static void toggleDisplay();
-    static int m_pinBeingRead;
+    static void toggle();
+    static int m_pinBeingRead;              //What index the button class is on 
 };
 
 
--- src/red.cpp
+++ src/red.cpp
@@ -1,17 +1,27 @@
+//---------------------------------------------//
+// Remote Display set up and functions
+// Author: Karen Su & Drew Gautier
+// Date: 4/28/2021
+//---------------------------------------------//
+// This file contains the definitions for the RED
+// class constructor and it's member functions.
+
+
 #include <red.h>
 #include <button.h>
 
+
+//Constructor creates RED object with button and lcd subobjects
 RED::RED (int rs, int en, int d4, int d5, int d6, int d7, int pinsToRead, int dispButton, int* pins8): lcd(rs, en, d4, d5, d6, d7), m_button(pinsToRead, dispButton) {
-  m_list = pins8;
+  m_list = pins8; //Assign the user's list of pins to a member variable
 }
 
+
+//Display the pin and the pin value on the lcd based on what index the button is on
 void RED::displayList(){
    lcd.clear();
    lcd.print("Pin A1:");
    lcd.setCursor(0,1);
    lcd.print(m_list[m_button.m_pinBeingRead]);
-   delay(1000);
+   delay(1000);//will want to remove this later and replace with a timer interupt
 }
-
-
-
